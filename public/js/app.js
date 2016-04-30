3249a1e17fa1d92b435eed4150c510eda967dd36
Mads Peter Jacobsen <madsissexy@gmail.com>
2016-04-30 15:01:55 +0200

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,12 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room') || 'Space';
+
+var $form = $('#message-form');
+var $message = $('#message');
+var $messages = $('#messages');
+
+console.log(name + ' wants to join ' + room);
 
 // socket on connect
 	socket.on('connect', function () {
@@ -11,18 +19,17 @@ var socket = io();
 		var momentTimestamp = moment.utc(message.timestamp);
 		var momentTimestampFormat = momentTimestamp.local().format('h:mm a');
 
-		$('#messages').append('<p><strong>' + momentTimestampFormat + ':</strong> ' + message.text + '</p>');
+		$messages.append('<p><strong>' + message.name + ' ' + momentTimestampFormat + '</strong></p>');
+		$messages.append('<p>' + message.text +'</p>');
 	});
 
 // handle submit of message
-	var $form = $('#message-form');
-	var $message = $('#message');
-
 	$form.on('submit', function (e) {
 		e.preventDefault();
 
 	// send message to server
 		socket.emit('message', {
+			name: name,
 			text: $message.val()
 		});
 
